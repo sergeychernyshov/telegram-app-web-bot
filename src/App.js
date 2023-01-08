81be472b7cf54a9d889ed6e7bad50da6e62ca757
chernyshov sergey <sergey_chernyshov_89@mail.ru>
2023-01-08 18:22:49 +0300

    set Product index

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Routes>
             <Route
                 index
-                element={<Form/>}
+                element={<ProductList/>}
             />
             <Route
                 path={'form'}
