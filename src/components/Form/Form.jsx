71ffe10a367d86d290be51bec16fe65120cf1458
chernyshov sergey <sergey_chernyshov_89@mail.ru>
2023-01-08 17:26:13 +0300

    change Form 4

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -25,7 +25,7 @@ const Form = () => {
     }, [onSendData])*/
 
     const sendDataToTelegram = () => {
-        tg.sendData(selectedRegions);
+        tg.sendData("sdsdfsdf");
     }
 
     useEffect(() => {
