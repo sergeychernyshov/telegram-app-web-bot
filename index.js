c86f7fadf350d23401267d971f6d8a9427d4471c
chernyshov sergey <sergey_chernyshov_89@mail.ru>
2023-01-08 12:24:38 +0300

    init telegram app web bot

---
+++ index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '5823953700:AAEfiI7UHgKrJgzZm5fCaNatgZTRa5NHX7k'
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text
+    if (text === '/start'){
+        await bot.sendMessage(chatId, 'Нажмите на кнопку заполинть форму',{
+            reply_markup: {
+                keyboard: [
+                    [{text: "Заполнить форму"}]
+                ]
+            }
+        });
+    }
+
+    if (text === '/go'){
+        await bot.sendMessage(chatId, 'Нажмите на кнопку заполнить форму2',{
+            reply_to_message_id: msg.message_id,
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                    [{ text: 'Поделится ботом', web_app: {url: 'https://ya.ru/'}}]
+                ]
+            })});
+    }
+
+});
